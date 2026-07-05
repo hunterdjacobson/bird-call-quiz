9190fced5d098cbe36a5d07f0a53c06f92d025d5
Hunter Jacobson <hunterdjacobson@gmail.com>
2026-07-05 17:31:13 -0400

    fix: import and add tailwindcss plugin to vite config array

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // Adds the Tailwind v4 compiler to Vite
+  ],
 })
